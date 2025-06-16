cd4208e3178a71bd2bf821ae5b2bfd3ad66db20e
Eugene Cheltsov <chill.icp@gmail.com>
2025-06-16 21:33:20 +0900

    nvim: nvimtree keys

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,3 @@
 require("options")
 require("configs.lazy")
+require("configs.mapping")
---
+++ nvim/lua/configs/mapping.lua
@@ -0,0 +1,4 @@
+local map = vim.keymap.set
+
+map("n", "<C-S-b>", "<cmd>NvimTreeToggle<CR>", { desc = "nvimtree toggle window" })
+map("n", "<leader>e", "<cmd>NvimTreeFocus<CR>", { desc = "nvimtree focus window" })
